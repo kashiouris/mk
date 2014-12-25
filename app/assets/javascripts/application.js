1899d124afba11b9cb826bb4b2201cc0c4cac6ad
Markos Kashiouris <kashiouris@gmail.com>
2014-12-25 15:17:33 -0500

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
 //= require turbolinks
 //= require_tree .
+//= require bootstrap
